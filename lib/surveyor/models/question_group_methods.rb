ab6e038a06f43db94442ca61bde893d388e817e9
Gernot Ullrich <gernot.ullrich@gmx.de>
2016-11-09 19:30:56 +0100

    Revert "try to fix layout problem"
    This reverts commit 1399f75da60fc57527db34a00f313195fd53412a.

--- lib/surveyor/models/question_group_methods.rb
+++ lib/surveyor/models/question_group_methods.rb
@@ -24,7 +24,6 @@ module Surveyor
       def default_args
         self.display_type ||= "inline"
         self.api_id ||= Surveyor::Common.generate_api_id
-        self.data_export_identifier ||= Surveyor::Common.normalize(title)
       end
 
       def renderer
